11530bfc08927553eb47c6b9dcb9b7335c20f2af
伪红学家 <pengwang.cmu@gmail.com>
2017-08-15 17:40:02 -0400

    Move getting song link logic to OnRequest

--- NeteaseReverseLadder/NeteaseProxy.cs
+++ NeteaseReverseLadder/NeteaseProxy.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Diagnostics;
 using System.Net;
 using System.Text.RegularExpressions;
@@ -48,54 +47,51 @@ namespace NeteaseReverseLadder
 
         public async Task OnRequest(object sender, SessionEventArgs e)
         {
+            if (e.WebSession.Request.Url.Contains("music.163.com/eapi/song/enhance") || e.WebSession.Request.Url.Contains("music.163.com/eapi/song/like"))
+            {
+                Console.WriteLine("从代理服务器获取：" + e.WebSession.Request.Url);
+                var proxy = proxySelector.GetTop();
+                var st = new Stopwatch();
+                st.Start();
+                try
+                {
+                    byte[] ret = null;
+                    using (var wc = new ImpatientWebClient())
+                    {
+                        wc.Proxy = new WebProxy(proxy.host, proxy.port);
+                        foreach (var aheader in e.WebSession.Request.RequestHeaders)
+                        {
+                            var str = aheader.Key.ToLower();
+                            if (str == "host" || str == "content-length" || str == "accept" || str == "user-agent" || str == "connection") continue;
+                            wc.Headers.Add(aheader.Key, aheader.Value.Value);
+                        }
+                        ret = wc.UploadData(e.WebSession.Request.Url.Replace("https://", "http://"), await e.GetRequestBody());
+                    }
+                    st.Stop();
+                    await e.Ok(ret);
+                    Console.WriteLine("修改完成，用时 " + st.ElapsedMilliseconds + " ms");
+                }
+                catch (Exception ex) { Console.WriteLine(ex); }
+            }
         }
 
         public async Task OnResponse(object sender, SessionEventArgs e)
         {
-            var responseHeaders = e.WebSession.Response.ResponseHeaders;
-            if (e.WebSession.Response.ContentType != null && (e.WebSession.Response.ContentType.Trim().ToLower().Contains("text") || e.WebSession.Response.ContentType.Trim().ToLower().Contains("json")) || e.WebSession.Request.Url.Contains("music.163.com/eapi/song"))
+            var contentType = e.WebSession.Response.ContentType;
+            if ((contentType.Contains("text") || contentType.Contains("json")) && e.WebSession.Request.Url.Contains("music.163.com/eapi/"))
             {
-                if (e.WebSession.Request.Url.Contains("music.163.com/eapi/song/enhance") || e.WebSession.Request.Url.Contains("music.163.com/eapi/song/like"))
+                var body = await e.GetResponseBodyAsString();
+                if (Regex.Match(body, "\"st\":-\\d+").Success)
                 {
-                    Console.WriteLine("从代理服务器获取：" + e.WebSession.Request.Url);
-                    var proxy = proxySelector.GetTop();
-                    var st = new Stopwatch();
-                    st.Start();
-                    byte[] ret = null;
-                    try
-                    {
-                        using (var wc = new ImpatientWebClient())
-                        {
-                            wc.Proxy = new WebProxy(proxy.host, proxy.port);
-                            foreach (var aheader in e.WebSession.Request.RequestHeaders)
-                            {
-                                var str = aheader.Key.ToLower();
-                                if (str == "host" || str == "content-length" || str == "accept" || str == "user-agent" || str == "connection") continue;
-                                wc.Headers.Add(aheader.Key, aheader.Value.Value);
-                            }
-                            ret = wc.UploadData(e.WebSession.Request.Url.Replace("https://", "http://"), await e.GetRequestBody());
-                        }
-                        st.Stop();
-                        await e.SetResponseBody(ret);
-                        Console.WriteLine("修改完成，用时 " + st.ElapsedMilliseconds + " ms");
-                    }
-                    catch (Exception ex) { Console.WriteLine(ex); }
-                }
-                else if (e.WebSession.Request.Url.Contains("music.163.com/eapi/"))
-                {
-                    var body = await e.GetResponseBodyAsString();
-                    if (Regex.Match(body, "\"st\":-\\d+").Success)
-                    {
-                        Console.WriteLine("替换歌曲列表信息");
-                        body = Regex.Replace(body, "\"st\":-\\d+", "\"st\":0");
-                        body = body.Replace("\"pl\":0", "\"pl\":320000");
-                        body = body.Replace("\"dl\":0", "\"dl\":320000");
-                        body = body.Replace("\"fl\":0", "\"fl\":320000");
-                        body = body.Replace("\"sp\":0", "\"sp\":7");
-                        body = body.Replace("\"cp\":0", "\"cp\":1");
-                        body = body.Replace("\"subp\":0", "\"subp\":1");
-                        await e.SetResponseBodyString(body);
-                    }
+                    Console.WriteLine("替换歌曲列表信息");
+                    body = Regex.Replace(body, "\"st\":-\\d+", "\"st\":0");
+                    body = body.Replace("\"pl\":0", "\"pl\":320000");
+                    body = body.Replace("\"dl\":0", "\"dl\":320000");
+                    body = body.Replace("\"fl\":0", "\"fl\":320000");
+                    body = body.Replace("\"sp\":0", "\"sp\":7");
+                    body = body.Replace("\"cp\":0", "\"cp\":1");
+                    body = body.Replace("\"subp\":0", "\"subp\":1");
+                    await e.SetResponseBodyString(body);
                 }
             }
         }
