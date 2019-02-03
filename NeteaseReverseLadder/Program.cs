a997df6c745676437347d9cf6656af7dd36e78e9
Peng Wang <pengwang@cmu.edu>
2019-02-03 19:53:41 +0800

    Make this great again!

--- NeteaseReverseLadder/Program.cs
+++ NeteaseReverseLadder/Program.cs
@@ -10,33 +10,33 @@ namespace NeteaseReverseLadder
             var ps = new ProxySelector();
             if (!UpdateProxySelector(ps))
             {
-                Console.WriteLine("获取代理列表失败");
+                Console.WriteLine("Failed retriving proxy list");
                 Console.ReadLine();
                 return;
             }
             var proxy = new NeteaseProxy(ps);
             proxy.Start();
-            Console.WriteLine("请设置网易云音乐代理为127.0.0.1，端口15213");
-            Console.WriteLine("如果播放失败，按回车切换到下一个代理服务器");
+            Console.WriteLine("Please change proxy of Netease Music to address 127.0.0.1，port 15213");
+            Console.WriteLine("Press enter to use the next proxy");
             while (true)
             {
                 var aproxy = ps.GetTop();
                 if (aproxy == null)
                 {
-                    Console.WriteLine("没有可用代理，重新搜索");
+                    Console.WriteLine("No available proxy, retrying");
                     proxy.Stop();
                     goto start;
                 }
-                Console.WriteLine("现在使用的是：" + aproxy);
+                Console.WriteLine("Using: " + aproxy);
                 Console.ReadLine();
                 ps.Remove(aproxy);
             }
         }
         static bool UpdateProxySelector(ProxySelector ps)
         {
-            Console.WriteLine("获取代理列表");
+            Console.WriteLine("Retriving proxy list");
             ps.UpdateList();
-            Console.WriteLine("共" + ps.Proxies.Count + "条结果，测试速度");
+            Console.WriteLine(ps.Proxies.Count + " results, measuring speed");
             ps.UpdateLatency();
             return ps.Proxies.Count >= 1 && ps.Proxies[0].latency != int.MaxValue;
         }
